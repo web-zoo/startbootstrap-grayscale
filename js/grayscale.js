4b1ac944728369927c22ce25bf139dddbabfb1d9
Spencer Mathews <spencer.mathews@gmail.com>
2016-04-12 23:25:33 -0700

    Add my Google API key and map to Davis

--- js/grayscale.js
+++ js/grayscale.js
@@ -50,7 +50,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(38.5449, -121.7405), // Davis
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -178,7 +178,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(38.5449, -121.7405);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
